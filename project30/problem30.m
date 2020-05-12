e2b4e599070e77e36d97b448333a31c32e29edf0
PaulBower <paulbower@live.com>
2020-05-12 10:12:41 -0400

    Update problem30.m
    Minor edits to comments

--- project30/problem30.m
+++ project30/problem30.m
@@ -4,22 +4,22 @@ function problem30(n)
 % as the sum of the "nth" power of their digits where n is the input
 % variable.
 %
-tic                                % Start timer
+tic % Start timer
 %
-outputSum = 0;       % Initialize output sum
+outputSum = 0;   % Initialize output sum
 maxNum = n*9^n;  % Compute maximum number to test
 for ii = 2:maxNum
     numDigitList = str2num(num2str(ii)');   
     % Convert the number to test to column vector of characters and then 
     % convert this vector of characters to a vector of numbers.
-    if ii == sum(numDigitList .^ n)          % Compute the sum of the numbers in the vector raised to the n power
-        outputSum = outputSum + ii;      % and compare this sum to the tested number; add it to the output sum if true.
+    if ii == sum(numDigitList .^ n)  % Compute the sum of the numbers in the vector raised to the n power
+        outputSum = outputSum + ii;  % and compare this sum to the tested number; add it to the output sum if true.
     end
 end
 %
 fprintf('The sum of all numbers which can be written as the sum of the %dth power of their digits is %d. \n \n', n, outputSum);
 %
-toc                                % Stop timer and output elapsed execution time
+toc % Stop timer and output elapsed execution time
 %
 end
 
